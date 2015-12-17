56cfce78c24b393d40e53d70737ba940c559f679
chenzhihao <cabtravis@gmail.com>
2015-12-17 15:54:04 +0800

    Add redux dev-tool

--- app/main.js
+++ app/main.js
@@ -3,19 +3,25 @@ import ReactDOM from 'react-dom';
 import TodoApp from './TodoApp.jsx';
 import './sass/index.scss';
 import '../bower_components/normalize-css/normalize.css';
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import thunkMiddleware from 'redux-thunk';
+import DevTools from './DevTool';
 import {Provider} from 'react-redux';
 import todoApp from './reducer';
 
-const createStoreWithMiddleware = applyMiddleware(
-    thunkMiddleware
+
+const finalCreateStore = compose(
+    applyMiddleware(thunkMiddleware),
+    DevTools.instrument()
 )(createStore);
 
 function main() {
     ReactDOM.render(
-        <Provider store={createStoreWithMiddleware(todoApp)}>
-            <TodoApp/>
+        <Provider store={finalCreateStore(todoApp)}>
+            <div>
+                <TodoApp/>
+                <DevTools/>
+            </div>
         </Provider>, document.getElementById('app'));
 }
 main();
